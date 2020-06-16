0efef70923c3f256688c1f37607f8345d82c3244
tairamatsuta <taira.matsuta@rakus.co.jp>
2020-06-16 16:04:20 +0900

    ユーザ登録の機能が完成しました。
    ユーザードメインクラスの実装
    
    ユーザーフォームクラスの実装
    
    ユーザー登録を行うコントローラーの実装
    
    入力値チェックの構文を追加
    
    ユーザ登録フォームの実装
    
    結果画面の実装

---
+++ src/main/java/com/example/controller/Exam04Controller.java
@@ -0,0 +1,56 @@
+package com.example.controller;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.domain.User;
+import com.example.form.UserForm;
+
+/**
+ * ユーザ登録を行うコントローラーの作成
+ * 
+ * @author taira.matsuta
+ *
+ */
+@Controller
+@RequestMapping("/exam04")
+public class Exam04Controller {
+	
+	@ModelAttribute
+	public UserForm setUpForm() {
+		return new UserForm();
+	}
+
+	@RequestMapping("")
+	public String index() {
+		return "exam04";
+	}
+	
+	@RequestMapping("/input")
+	public String input(
+			@Validated UserForm form
+			, BindingResult result
+			, RedirectAttributes redirectAttributes
+			) {
+		if(result.hasErrors()) {
+			return index();
+		}
+		
+		User user = new User();
+		BeanUtils.copyProperties(form, user);
+		
+		redirectAttributes.addFlashAttribute("user", user);
+		
+		return "redirect:/exam04/result";
+	}
+	
+	@RequestMapping("/result")
+	public String result() {
+		return "exam04-result";
+	}
+}
