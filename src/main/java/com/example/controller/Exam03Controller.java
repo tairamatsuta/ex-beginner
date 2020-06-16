16e3e706bc95faedf6466f45f4da2a102aa214ba
tairamatsuta <taira.matsuta@rakus.co.jp>
2020-06-16 16:01:39 +0900

    Javadocコメントの追加

--- src/main/java/com/example/controller/Exam03Controller.java
+++ src/main/java/com/example/controller/Exam03Controller.java
@@ -6,20 +6,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 合計金額を計算するコントローラー
+ * 
+ * @author taira.matsuta
+ *
+ */
 @Controller
 @RequestMapping("/exam03")
 public class Exam03Controller {
 	
+	/** 税率 */
 	private static final double TAX = 1.1;
 
+	/**
+	 * アプリケーションスコープの準備
+	 */
 	@Autowired
 	private ServletContext application;
 	
+	/**
+	 * @return　金額入力画面へ遷移
+	 */
 	@RequestMapping("")
 	public String index() {
 		return "exam03";
 	}
 	
+	/**
+	 * 合計金額の計算
+	 * 
+	 * @param item1 商品1の価格
+	 * @param item2 商品2の価格
+	 * @param item3 商品3の価格
+	 * @return　合計金額表示画面へ遷移
+	 */
 	@RequestMapping("/input")
 	public String input(String item1, String item2, String item3) {
 		Integer item1Price = Integer.parseInt(item1);
